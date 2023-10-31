8a690be87de6df880cace6b037dbaccd0d120598
manon zheng <manon.zheng@oocl.com>
2023-10-31 16:38:13 +0800

    feat: change build public path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const publicPath = isProduction ? 'https://static-lessismore.oss-cn-shanghai.aliyuncs.com/assets' : '/';
+
 export default defineConfig({
   plugins: [vue()],
+  base: publicPath,
 })
