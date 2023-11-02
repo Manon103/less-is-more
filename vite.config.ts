eafc6f78338627debb2888d5ffdb40b8be8e2733
manon zheng <manon.zheng@oocl.com>
2023-11-02 14:03:23 +0800

    feat: split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { splitVendorChunkPlugin } from 'vite';
 
 const isProduction = process.env.NODE_ENV === 'production';
 // const publicPath = isProduction ? 'https://static-lessismore.oss-cn-shanghai.aliyuncs.com' : '/';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin()],
   // base: publicPath,
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const arr = id.toString().split('node_modules/')[1].split('/');
+            switch(arr[0]) {
+              case '@vue':
+              case 'vue':
+                return 'vue';
+              case 'element-plus':
+              case '@element-plus':
+                return 'elementPlus';
+              default:
+                return 'vendor';
+            }
+          }
+        }
+      }
+    }
+  }
 })
